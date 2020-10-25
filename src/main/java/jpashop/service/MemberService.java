38962dbdd3457a15e03816cf8f80828c196e24ce
youngran <horany83@gmail.com>
2020-10-25 17:23:26 +0900

    ADD 20201025_02

---
+++ src/main/java/jpashop/service/MemberService.java
@@ -0,0 +1,50 @@
+package jpashop.service;
+
+import jpashop.domain.Member;
+import jpashop.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class MemberService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    /**
+    * 회원가입
+    */
+    public Long join(Member member) {
+        validateDupicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDupicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 번호로 회원조회
+     * @param memberId
+     * @return
+     */
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+}
